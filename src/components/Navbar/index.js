96a591abe471de24a7dc92f9acfc5c8a6eefe1e5
Kian Sam <kiansamadani@Kians-MacBook-Pro.local>
2021-04-05 15:52:02 +0300

    Second commit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {FaBars, Fatimes} from "react-icons/fa";
 import { IconContext } from "react-icons/lib"
 
+const Navbar = () => {
+
+    const [click, setClick] = useState(false);
+    const [scroll, setScroll] = useState(false);
+
+    const changeNav = () => {
+        if(window.scroll >= 80) {
+            setScroll(true)
+        }else {
+            setScroll(false)
+        }
+    }
+
+    useEffect(() => {
+        changeNav()
+   window.addEventListener("scroll", changeNav)
+    }, [])
+    return (
+        <>
+       <IconContext.provider value={{ color: "#141414"}} >
+       <Nav active={scroll} click={click}>
+
+       </Nav>
+       </IconContext.provider>
+         </>
+    )
+
+}
+export default Navbar;
